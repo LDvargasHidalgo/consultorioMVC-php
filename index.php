fe40db5028c94b5366c70b0d484168b132baf366
Ronald Rosero <ronaldes93@gmail.com>
2022-03-25 14:08:37 -0500

    rutes

--- index.php
+++ index.php
@@ -1,5 +1,48 @@
 <?php
-require_once "controlador/plantilla.controlador.php";
-$plantilla = new ControladorPlantilla();
-$plantilla->ctrPlantilla();
+// require_once "controlador/plantilla.controlador.php";
+// $plantilla = new ControladorPlantilla();
+// $plantilla->ctrPlantilla();
+
+$url = !empty($_GET['url']) ? $_GET['url'] :'inicio/inicio' ;
+$arrUrl = explode('/', $url);
+$controller = $arrUrl [0];
+$method = $arrUrl [0];
+$params = '';
+
+if (!empty($arrUrl [1])) 
+{
+    if ($arrUrl [1] != "") 
+    {
+        $method = $arrUrl [1];
+    }
+
+    
+}
+
+if (!empty($arrUrl [2])) 
+{
+    if ($arrUrl [2] != "") 
+    {
+        for ($i=2; $i < count($arrUrl); $i++) { 
+            $params .= $arrUrl[$i] . ',';
+           
+        }
+        $params = trim($params,',');
+        
+       
+    }
+
+    
+}
+echo '<br>';
+echo "controllador: " . $controller;
+echo '<br>';
+echo 'metodo: ' . $method;
+echo '<br>';
+echo 'parametros: ' . $params;
+
+
+
+
 ?>
+
